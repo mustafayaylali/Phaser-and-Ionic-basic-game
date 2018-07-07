c9bac12383dc66a7d54f44f3a78d43379b02d52a
mustafayaylali <b140910051@sakarya.edu.tr>
2018-07-07 19:58:26 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { OyunPage } from '../pages/oyun/oyun';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = OyunPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+
       statusBar.styleDefault();
       splashScreen.hide();
     });
